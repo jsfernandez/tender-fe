45ec7c63deceb5300df51a8edc1b0f8a4e12b186
Javier Fernandez <jfern.emp@gmail.com>
2022-12-13 18:15:20 -0300

    chore: fixing front

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -7,6 +7,7 @@ import {
 import {
   Alert,
   Card,
+  Collapse,
   Descriptions,
   Divider,
   Drawer,
@@ -28,6 +29,7 @@ import { BUDGE_TYPE_LABEL } from "../../const/budge";
 import { PAYMENT_METHOD_LABEL } from "../../const/paymentMethod";
 import { UNITS_TIME_LABEL } from "../../const/unitsTime";
 
+const { Panel } = Collapse;
 const { Search } = Input;
 
 const Dashboard = () => {
@@ -49,7 +51,8 @@ const Dashboard = () => {
 
   useEffect(() => {
     if (isLoading === false && data && data.Listado) {
-      setTenderList(data.Listado);
+      const result = data.Listado;
+      setTenderList(result || []);
     }
   }, [data, isLoading]);
 
@@ -75,6 +78,9 @@ const Dashboard = () => {
       } catch (error) {
         console.log({ error });
       }
+    } else {
+      const result = data?.Listado;
+      setTenderList(result || []);
     }
   }, [querySearch, data]);
 
@@ -186,88 +192,98 @@ const Dashboard = () => {
               <p className="tender-type-text">
                 * {TENDER_TYPES_LABELS[tender.Tipo]} *
               </p>
-              <p className="tender-description-text">{tender.Descripcion}</p>
-              <ul className="tender-ul-description">
-                <li>
-                  Periodos de evaluación:{" "}
-                  {UNITS_TIME_LABEL[tender.UnidadTiempoEvaluacion]}
-                </li>
-                <li>
-                  Duración de contrato:{" "}
-                  {tender.TiempoDuracionContrato} {UNITS_TIME_LABEL[tender.UnidadTiempoDuracionContrato]}
-                </li>
-                <li>
-                  Modalidad de pago: {PAYMENT_METHOD_LABEL[tender.Modalidad]}
-                </li>
-              </ul>
-              <p className="tender-amout-info">
-                {BUDGE_TYPE_LABEL[tender.Estimacion]}&nbsp; $
-                {transformToCurrency(tender.MontoEstimado || 0)} {tender.Moneda}
-              </p>
-              {tender && tender.Items.Cantidad > 0 && (
-                <>
-                  <Divider>Productos requeridos ({tender.Items.Cantidad})</Divider>
-                  {tender.Items.Listado.map((item) => {
-                    return (
-                      <>
-                        <Descriptions title="" bordered>
+              <Collapse defaultActiveKey={['1']}>
+                <Panel header="Descripción" key="1">
+                  <p className="tender-description-text">{tender.Descripcion}</p>
+                  <ul className="tender-ul-description">
+                    <li>
+                      Periodos de evaluación:{" "}
+                      {UNITS_TIME_LABEL[tender.UnidadTiempoEvaluacion]}
+                    </li>
+                    <li>
+                      Duración de contrato:{" "}
+                      {tender.TiempoDuracionContrato} {UNITS_TIME_LABEL[tender.UnidadTiempoDuracionContrato]}
+                    </li>
+                    <li>
+                      Modalidad de pago: {PAYMENT_METHOD_LABEL[tender.Modalidad]}
+                    </li>
+                    <li>
+                      Se permite Sub-Contratacion: {tender.SubContratacion ? 'Si' : 'No'}
+                    </li>
+                  </ul>
+                  <p className="tender-amout-info">
+                    {BUDGE_TYPE_LABEL[tender.Estimacion]}&nbsp; $
+                    {transformToCurrency(tender.MontoEstimado || 0)} {tender.Moneda}
+                  </p>
+                </Panel>
+                {tender && tender.Items.Cantidad > 0 && (
+                  <Panel header={`Productos requeridos (${tender.Items.Cantidad})`} key="2">
+                    {tender.Items.Listado.map((item) => {
+                      return (
+                        <>
+                          <Descriptions title="" bordered key={item.CodigoProducto}>
                             <Descriptions.Item label="Nombre del Producto" span={3}>
-                            {item.NombreProducto}
-                          </Descriptions.Item>
-                          <Descriptions.Item label="Codigo del Producto" span={3}>
-                            {item.CodigoProducto}
-                          </Descriptions.Item>
-                          <Descriptions.Item label="Descripción" span={3}>
-                            {item.Descripcion}
-                          </Descriptions.Item>
-                          <Descriptions.Item label="Codigo Categoria" span={3}>
-                            {item.CodigoCategoria}
-                          </Descriptions.Item>
-                          <Descriptions.Item label="Categoria" span={3}>
-                            {item.Categoria}
-                          </Descriptions.Item>
-                          <Descriptions.Item label="Cantidad" span={3}>
-                            {item.Cantidad} {item.UnidadMedida}
-                          </Descriptions.Item>
-                        </Descriptions>
-                        <br />
-                      </>
-                    );
-                  })}
-                </>
-              )}
-              <Divider />
-              <Descriptions title="Información del comprador" bordered>
-                <Descriptions.Item label="Codigo del Organismo" span={3}>
-                  {tender.Comprador.CodigoOrganismo}
-                </Descriptions.Item>
-                <Descriptions.Item label="Nombre" span={3}>
-                  {tender.Comprador.NombreOrganismo}
-                </Descriptions.Item>
-                <Descriptions.Item label="Nombre de Unidad" span={3}>
-                  {tender.Comprador.NombreUnidad}
-                </Descriptions.Item>
-                <Descriptions.Item label="Codigo de Unidad" span={3}>
-                  {tender.Comprador.RutUnidad}
-                </Descriptions.Item>
-                <Descriptions.Item label="Rut de Unidad" span={3}>
-                  {tender.Comprador.CodigoUnidad}
-                </Descriptions.Item>
-                <Descriptions.Item label="Dirección" span={3}>
-                  {tender.Comprador.DireccionUnidad},{" "}
-                  {tender.Comprador.ComunaUnidad},{" "}
-                  {tender.Comprador.RegionUnidad}
-                </Descriptions.Item>
-                <Descriptions.Item label="Usuario" span={3}>
-                  {tender.Comprador.NombreUsuario}
-                </Descriptions.Item>
-                <Descriptions.Item label="Rut de usuario" span={3}>
-                  {tender.Comprador.RutUsuario}
-                </Descriptions.Item>
-                <Descriptions.Item label="Cargo" span={3}>
-                  {tender.Comprador.CargoUsuario}
-                </Descriptions.Item>
-              </Descriptions>
+                              {item.NombreProducto}
+                            </Descriptions.Item>
+                            <Descriptions.Item label="Codigo del Producto" span={3}>
+                              {item.CodigoProducto}
+                            </Descriptions.Item>
+                            <Descriptions.Item label="Descripción" span={3}>
+                              {item.Descripcion}
+                            </Descriptions.Item>
+                            <Descriptions.Item label="Codigo Categoria" span={3}>
+                              {item.CodigoCategoria}
+                            </Descriptions.Item>
+                            <Descriptions.Item label="Categoria" span={3}>
+                              {item.Categoria}
+                            </Descriptions.Item>
+                            <Descriptions.Item label="Cantidad" span={3}>
+                              {item.Cantidad} {item.UnidadMedida}
+                            </Descriptions.Item>
+                          </Descriptions>
+                          <br />
+                        </>
+                      );
+                    })}
+                  </Panel>
+                )}
+                <Panel header="Información del comprador" key="3">
+                  <Descriptions title="" bordered>
+                    <Descriptions.Item label="Codigo del Organismo" span={3}>
+                      {tender.Comprador.CodigoOrganismo}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Nombre" span={3}>
+                      {tender.Comprador.NombreOrganismo}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Nombre de Unidad" span={3}>
+                      {tender.Comprador.NombreUnidad}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Codigo de Unidad" span={3}>
+                      {tender.Comprador.RutUnidad}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Rut de Unidad" span={3}>
+                      {tender.Comprador.CodigoUnidad}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Dirección" span={3}>
+                      {tender.Comprador.DireccionUnidad},{" "}
+                      {tender.Comprador.ComunaUnidad},{" "}
+                      {tender.Comprador.RegionUnidad}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Usuario" span={3}>
+                      {tender.Comprador.NombreUsuario}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Rut de usuario" span={3}>
+                      {tender.Comprador.RutUsuario}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Cargo" span={3}>
+                      {tender.Comprador.CargoUsuario}
+                    </Descriptions.Item>
+                  </Descriptions>
+                </Panel>
+              </Collapse>
+
+
+
             </div>
           )}
         </Drawer>
